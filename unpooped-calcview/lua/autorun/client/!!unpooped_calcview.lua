71502fc01669662bbef1f1484a45246d4fb7762b
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2025-04-26 02:26:26 +0500

    OVERENGINEERING MY BELOVED

--- unpooped-calcview/lua/autorun/client/!!unpooped_calcview.lua
+++ unpooped-calcview/lua/autorun/client/!!unpooped_calcview.lua
@@ -37,44 +37,16 @@ function VMData( )
 	return setmetatable( { }, _VMData )
 end
 
-local function CalcViewOverride( Player, Origin, Angles, FOV, ZNear, ZFar, ... )
-	local Vehicle = Player:GetVehicle( )
-
-	local View = {
-		["origin"] = Origin,
-		["angles"] = Angles,
-		["fov"] = FOV,
-		["znear"] = ZNear,
-		["zfar"] = ZFar,
-		["drawviewer"] = false
-	}
-
-	if ( IsValid( Vehicle ) ) then
-		return hook.Run( "CalcVehicleView", Vehicle, Player, View ) 
-	end
-
-	local Weapon = Player:GetActiveWeapon( )
-
-	if ( IsValid( Weapon ) ) then
-		local Function = Weapon.CalcView
-		if ( Function ) then
-			local WeaponOrigin, WeaponAngles, WeaponFOV = Function( Weapon, Player, Vector( View.origin ), Angle( View.angles ), View.fov )
-
-			View.origin = WeaponOrigin or View.origin
-			View.angles = WeaponAngles or View.angles
-			View.fov = WeaponFOV or View.fov
-		end
-	end
-
+local function RunCalcViewEx( Player, Origin, Angles, FOV, ZNear, ZFar )
 	local Data = CVData( )
 
 	Data:SetPlayer( Player )
-	Data:SetOrigin( View.origin )
-	Data:SetAngles( View.angles )
-	Data:SetFOV( View.fov )
-	Data:SetZNear( View.znear )
-	Data:SetZFar( View.zfar )
-	Data:SetDrawViewer( View.drawviewer )
+	Data:SetOrigin( Origin )
+	Data:SetAngles( Angles )
+	Data:SetFOV( FOV )
+	Data:SetZNear( ZNear )
+	Data:SetZFar( ZFar )
+	Data:SetDrawViewer( false )
 	Data:SetOverride( false )
 
 	local ExHooks = hook.GetTable( )[ "CalcViewEx" ]
@@ -88,58 +60,10 @@ local function CalcViewOverride( Player, Origin, Angles, FOV, ZNear, ZFar, ... )
 		end
 	end
 
-	View.origin = Data:GetOrigin( )
-	View.angles = Data:GetAngles( )
-	View.fov = Data:GetFOV( )
-	View.znear = Data:GetZNear( )
-	View.zfar = Data:GetZFar( )
-	View.drawviewer = Data:GetDrawViewer( )
-
-	local Hooks = hook.GetTable( )[ "CalcView" ]
-
-	if ( Hooks ) then
-		for Name, Function in pairs( Hooks ) do
-			local HookView = Function( Player, View.origin, View.angles, View.fov, View.znear, View.zfar, ... )
-
-			if HookView then
-				return HookView
-			end
-		end
-	end
-
-	// player_manager.RunClass( Player, "CalcView", View, ... )
-
-	if ( drive.CalcView( Player, View ) ) then
-		return View 
-	end
-
-	return View
+	return Data:GetPlayer( ), Data:GetOrigin( ), Data:GetAngles( ), Data:GetFOV( ), Data:GetZNear( ), Data:GetZFar( ), Data:GetDrawViewer( )
 end
 
-local function CalcViewModelViewOverride( Weapon, Viewmodel, OldPosition, OldAngles, Position, Angles )
-	//local ReturnPosition = Vector( OldPosition )
-	//local ReturnAngles = Angle( OldAngles )
-
-	if ( IsValid( Weapon ) ) then
-		local Function = Weapon.CalcViewModelView
-		if ( Function ) then
-			local FuncPosition, FuncAngles = Function( Weapon, Viewmodel, OldPosition, OldAngles, Position, Angles )
-			if ( FuncPosition and FuncAngles ) then
-				Position = FuncPosition
-				Angles = FuncAngles
-			end
-		end
-
-		local Function2 = Weapon.GetViewModelPosition
-		if ( Function2 ) then
-			local FuncPosition, FuncAngles = Function2( Weapon, Position, Angles )
-			if ( FuncPosition and FuncAngles ) then
-				Position = FuncPosition
-				Angles = FuncAngles
-			end
-		end
-	end
-
+local function RunCalcViewModelViewEx( Weapon, Viewmodel, OldPosition, OldAngles, Position, Angles )
 	local Data = VMData( )
 
 	Data:SetWeapon( Weapon )
@@ -160,37 +84,28 @@ local function CalcViewModelViewOverride( Weapon, Viewmodel, OldPosition, OldAng
 		end
 	end
 
-	Position = Data:GetPosition( )
-	Angles = Data:GetAngles( )
-
-	local Hooks = hook.GetTable( )[ "CalcViewModelView" ]
-
-	if ( Hooks ) then
-		for Name, Function in pairs( Hooks ) do
-			local HookPosition, HookAngles = Function( Weapon, Viewmodel, OldPosition, OldAngles, Position, Angles )
-
-			if ( HookPosition && HookAngles ) then
-				return HookPosition, HookAngles
-			end
-		end
-	end
-
-	return Position, Angles
+	return Data:GetWeapon( ), Data:GetViewmodel( ), Data:GetOldPosition( ), Data:GetOldAngles( ), Data:GetPosition( ), Data:GetAngles( )
 end
 
 local HookCallOriginal = hook.Call
-hook.Call = function( name, gm, ... )
-	if name == "CalcView" then
-		local a, b, c, d, e, f = CalcViewOverride( ... )
-		return a, b, c, d, e, f
-	end
-	
-	if name == "CalcViewModelView" then
-		local a, b, c, d, e, f = CalcViewModelViewOverride( ... )
-		return a, b, c, d, e, f
+hook.Call = function( Name, Gamemode, ... )
+	if Name == "CalcView" then
+		local Player, Origin, Angles, FOV, ZNear, ZFar, DrawViewer = RunCalcViewEx( ... )
+
+		local Result = HookCallOriginal( Name, Gamemode, Player, Origin, Angles, FOV, ZNear, ZFar )
+
+		Result["drawviewer"] = DrawViewer or Result["drawviewer"]
+
+		return Result
 	end
 
-	return HookCallOriginal( name, gm, ... )
+	if Name == "CalcViewModelView" then
+		local Weapon, Viewmodel, OldPosition, OldAngles, Position, Angles = RunCalcViewModelViewEx( ... )
+
+		return HookCallOriginal( Name, Gamemode, Weapon, Viewmodel, OldPosition, OldAngles, Position, Angles )
+	end
+
+	return HookCallOriginal( Name, Gamemode, ... )
 end
 
 hook.Add( "Initialize", "jeff_the_killer_came_to_me_in_my_sleep_and_said_skibidi_dop_dop_yes_yes", function( )
